56242d09f79c222d3da11805585a120028f7e9e8
mazoku1999 <rogelio.carata10@gmail.com>
2026-01-27 16:18:55 -0400

    iniciooo

---
+++ packages/js-sdk/src/index.ts
@@ -0,0 +1,23 @@
+// classes
+export { LiveAvatarSession } from "./LiveAvatarSession";
+
+// types
+export type {
+  SessionConfig,
+  SessionInfo,
+  SessionApiError,
+} from "./LiveAvatarSession";
+export type { VoiceChat, VoiceChatConfig } from "./VoiceChat";
+
+// enums
+export { ConnectionQuality } from "./QualityIndicator";
+export {
+  SessionEvent,
+  SessionState,
+  Language,
+  SessionDisconnectReason,
+  AgentEventsEnum,
+  AgentEvent,
+  CommandEventsEnum,
+} from "./LiveAvatarSession";
+export { VoiceChatState, VoiceChatEvent } from "./VoiceChat";
